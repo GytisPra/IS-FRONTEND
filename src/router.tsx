a079cdafd4cb7319fe118fade03997bb8eb498b5
Gytis Pranauskas <gpgytis@gmail.com>
2024-11-09 19:02:31 +0200

    removed css

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import "./index.css";
 import Home from "./home/home";
+import EventManagement from "./event-management/EventManagement";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/event-management",
+    element: <EventManagement />,
   },
 ]);
