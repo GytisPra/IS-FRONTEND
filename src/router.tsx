f2561159240e2aebc613a58d93d2d5be51467bdc
gedvilad <gedvilad@gmail.com>
2024-11-09 21:01:27 +0200

    added ticket creation form

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import "./index.css";
 import Home from "./home/home";
 import EventManagement from "./event-management/EventManagement";
+import CreatingTicket from "./ticket-buying/CreatingTicket";
 
 export const router = createBrowserRouter([
   {
@@ -11,5 +12,9 @@ export const router = createBrowserRouter([
   {
     path: "/event-management",
     element: <EventManagement />,
+  },
+  {
+    path: "/creating-ticket",
+    element: <CreatingTicket />,
   },
 ]);
